34f2bcbe8456802dea2ef18343814c07eabc26d5
JeremySky <86520270+JeremySky93@users.noreply.github.com>
2023-11-08 08:56:30 -0800

    Create HealthManager class, import HealthKit

---
+++ BeActiv/HealthManager.swift
@@ -0,0 +1,8 @@
+//
+//  HealthManager.swift
+//  BeActiv
+//
+//  Created by Jeremy Manlangit on 11/8/23.
+//
+
+import Foundation
---
+++ BeActiv/MainTabView.swift
@@ -0,0 +1,20 @@
+//
+//  TabView.swift
+//  BeActiv
+//
+//  Created by Jeremy Manlangit on 11/8/23.
+//
+
+import SwiftUI
+
+struct TabView: View {
+    @State var selectedTab = "Home"
+    
+    var body: some View {
+        TabView
+    }
+}
+
+#Preview {
+    TabView()
+}
