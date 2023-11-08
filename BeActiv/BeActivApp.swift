74fe7f72551bfffa8761551bebb05098a894ce79
JeremySky <86520270+JeremySky93@users.noreply.github.com>
2023-11-08 08:17:51 -0800

    Initial Commit

---
+++ BeActiv/BeActivApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeActivApp.swift
+//  BeActiv
+//
+//  Created by Jeremy Manlangit on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct BeActivApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
