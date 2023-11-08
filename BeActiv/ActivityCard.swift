82816149e222f77ec896ce22003729ee4ad8acd0
JeremySky <86520270+JeremySky93@users.noreply.github.com>
2023-11-08 09:32:58 -0800

    build fetchTodaySteps query

--- BeActiv/ActivityCard.swift
+++ BeActiv/ActivityCard.swift
@@ -7,7 +7,20 @@
 
 import SwiftUI
 
+struct Activity {
+    let id: Int
+    let title: String
+    let subtitle: String
+    let image: String
+    let amount: String
+}
+
+extension Activity {
+    static var test = Activity(id: 0, title: "Daily steps", subtitle: "Goal: 10,000", image: "figure.walk", amount: "5,677")
+}
+
 struct ActivityCard: View {
+    @State var activity: Activity
     var body: some View {
         
         ZStack {
@@ -17,21 +30,21 @@ struct ActivityCard: View {
             VStack(spacing: 20) {
                 HStack(alignment: .top) {
                     VStack(alignment: .leading, spacing: 5) {
-                        Text("Daily steps")
+                        Text(activity.title)
                             .font(.system(size: 16))
                         
-                        Text("Goal: 10,000")
+                        Text(activity.subtitle)
                             .font(.system(size: 12))
                             .foregroundStyle(.gray)
                     }
                     
                     Spacer()
                     
-                    Image(systemName: "figure.walk")
+                    Image(systemName: activity.image)
                         .foregroundColor(.green)
                 }
                 
-                Text("6,489")
+                Text(activity.amount)
                     .font(.system(size: 24))
             }
             .padding()
@@ -40,5 +53,5 @@ struct ActivityCard: View {
 }
 
 #Preview {
-    ActivityCard()
+    ActivityCard(activity: Activity.test)
 }
--- BeActiv/HealthManager.swift
+++ BeActiv/HealthManager.swift
@@ -8,9 +8,17 @@
 import Foundation
 import HealthKit
 
+extension Date {
+    static var startOfDay: Date {
+        Calendar.current.startOfDay(for: Date())
+    }
+}
+
 class HealthManager: ObservableObject {
     
     let healthStore = HKHealthStore()
+    
+    @Published var activities: [String: Activity] = [:]
     
     init() {
         let steps = HKQuantityType(.stepCount)
@@ -24,5 +32,37 @@ class HealthManager: ObservableObject {
                 print("error fetching health data")
             }
         }
+    }
+    
+    func fetchTodaySteps() {
+        let steps = HKQuantityType(.stepCount)
+        let predicate = HKQuery.predicateForSamples(withStart: .startOfDay, end: Date())
+        let query = HKStatisticsQuery(quantityType: steps, quantitySamplePredicate: predicate) { _, result, error in
+            guard let quantity = result?.sumQuantity(), error == nil else {
+                print("error fetching todays step data")
+                return
+            }
+            
+            let stepCount = quantity.doubleValue(for: .count())
+            let activity = Activity(id: 0, title: "Today steps", subtitle: "Goal 10,000", image: "figure.walk", amount: stepCount.formattedString())
+            DispatchQueue.main.async {
+                self.activities["todaySteps"] = activity
+            }
+            
+            
+            print(stepCount)
+        }
+        
+        healthStore.execute(query)
+    }
+}
+
+extension Double {
+    func formattedString() -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.maximumFractionDigits = 0
+        
+        return numberFormatter.string(from: NSNumber(value: self))!
     }
 }
