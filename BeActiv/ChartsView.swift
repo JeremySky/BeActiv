e4b20501d5ec0452324201eb52dbe7208a48875f
JeremySky <86520270+JeremySky93@users.noreply.github.com>
2023-11-09 14:40:10 -0800

    add charts

---
+++ BeActiv/ChartsView.swift
@@ -0,0 +1,18 @@
+//
+//  ChartsView.swift
+//  BeActiv
+//
+//  Created by Jeremy Manlangit on 11/8/23.
+//
+
+import SwiftUI
+
+struct ChartsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChartsView()
+}
